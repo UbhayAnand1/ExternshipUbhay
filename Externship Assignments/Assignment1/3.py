aeca8c25b46b5c9f2d5ca60c7871851e59a9fb60
UbhayAnand1 <100370172+UbhayAnand1@users.noreply.github.com>
2023-05-28 20:06:22 +0530

    Delete 3.py

--- Externship Assignments/Assignment1/3.py
+++
@@ -1,9 +0,0 @@
-def multiply(num1, num2):
-    result = num1 * num2
-    return result
-
-num1 = 23
-num2 = 10
-
-answer = multiply(num1, num2)
-print("The result of multiplying", num1, "and", num2, "is:", answer)
